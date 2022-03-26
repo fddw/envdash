11232f9d3a13305c65858dc2bebaa0827f790d6d
Fionnuala <f.diegowatson@gmail.com>
2022-03-26 13:34:03 +0000

    initial commit

---
+++ styles/home.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  background-color: seagreen;
+`;
